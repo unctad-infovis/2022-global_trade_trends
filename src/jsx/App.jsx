ecf93128cb2772927774b07866b47cfa2fbcc2cb
Teemo Tebest <teemo.tebest@gmail.com>
2022-12-14 17:53:08 +0100

    Initial version

---
+++ src/jsx/App.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+
+// Load helpers.
+import { transpose } from 'csv-transpose';
+import CSVtoJSON from './helpers/CSVtoJSON.js';
+import ChartLine from './components/ChartLine.jsx';
+
+import '../styles/styles.less';
+
+function App() {
+  // Data states.
+  const [dataFigure, setDataFigure] = useState(false);
+
+  const cleanData = (data) => data.map((el, i) => {
+    const labels = Object.keys(el).filter(val => val !== 'Name').map(val => val);
+    const values = Object.values(el).map(val => parseFloat(val) * 100).filter(val => !Number.isNaN(val));
+    return ({
+      data: values.map((val, j) => ({
+        dataLabels: {
+          y: (i === 0) ? -10 : 30
+        },
+        name: labels[j],
+        y: val
+      })),
+      labels,
+      name: el.Name
+    });
+  });
+
+  useEffect(() => {
+    const data_file = `${(window.location.href.includes('unctad.org')) ? 'https://storage.unctad.org/2022-plastic_trade/' : './'}assets/data/2022-global_trade_trends_figure_12.csv`;
+    try {
+      fetch(data_file)
+        .then((response) => {
+          if (!response.ok) {
+            throw Error(response.statusText);
+          }
+          return response.text();
+        })
+        .then(body => setDataFigure(cleanData(CSVtoJSON(transpose(body)))));
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  return (
+    <div className="app">
+      {dataFigure && (
+      <ChartLine
+        idx="12"
+        data={dataFigure}
+        note=""
+        source="UNCTAD calculations based on national statistics."
+        subtitle="Global trade annual trends in good and services between 2018 and 2022"
+        suffix="%"
+        title="Global trade is slowing down"
+        ylabel=""
+      />
+      )}
+      <noscript>Your browser does not support JavaScript!</noscript>
+    </div>
+  );
+}
+
+export default App;
